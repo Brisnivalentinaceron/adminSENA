be842a9a38041a489d0364b5af47ca3424891ad4
brisni <brisni.ceron@gmail.com>
2025-05-15 17:11:14 -0500

    RelacionModelo-Confioenti2

--- app/Models/Computer.php
+++ app/Models/Computer.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Courses_Teacher extends Model
+class Computer extends Model
 {
-    use HasFactory;
+    public function apprentice()
+    {
+        return $this->hasOne(Apprentice::class);
+    }
 }
